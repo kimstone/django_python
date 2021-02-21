91771ce1e73c1a4ec3af83349c8ca58083b6ef5a
kimstone <kimstone.va@gmail.com>
2021-02-20 23:12:56 -0500

    created user model

--- apps/travel_prep/models.py
+++ apps/travel_prep/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
+import re
 
 # Create your models here.
+
+class UserManager(models.Manager):
+    pass
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    email = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
--- apps/travel_prep/views.py
+++ apps/travel_prep/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+import bcrypt
 
 from . models import *
 
